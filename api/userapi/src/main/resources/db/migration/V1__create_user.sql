cfebc6146d7a44bb3dccdd88fc7032f0ca27d396
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-04-13 13:38:53 -0300

    alterando banco de dados

--- api/userapi/src/main/resources/db/migration/V1__create_user.sql
+++ api/userapi/src/main/resources/db/migration/V1__create_user.sql
@@ -2,7 +2,8 @@ CREATE TABLE tb_user (
     id int8 NOT NULL,
     name varchar(255) NOT NULL,
     age integer NOT NULL,
+    cpf varchar(13) NULL,
     constraint user_pk primary key (id)
 );
 
-insert into tb_user(id, name, age) values ('1', 'Darlan', 33);
+insert into tb_user(id, name, age, cpf) values ('1', 'Darlan', 33, '35919484896');
