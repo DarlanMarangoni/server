99703743aa337a34e9d9724c1ad750d4ae5dca1f
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-04-14 15:12:43 -0300

    renomeando expense

--- api/financeapi/src/main/kotlin/com/darlanmarangoni/financeapi/domain/BankStatement.kt
+++ api/financeapi/src/main/kotlin/com/darlanmarangoni/financeapi/domain/BankStatement.kt
@@ -4,17 +4,22 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.*
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
+import java.math.BigDecimal
 import java.time.LocalDateTime
 
 @Entity
-@Table(name = "tb_category")
-class Category(
+@Table(name = "tb_bank_statement")
+class BankStatement(
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     val id: Long? = null,
     @Column(name = "name", nullable = false)
     val name: String,
+    @Column(name = "description", nullable = false)
+    val description: String,
+    @Column(name = "value", nullable = false)
+    val value: BigDecimal,
     @UpdateTimestamp
     @Column(name = "updated_date")
     @JsonIgnore
@@ -22,5 +27,8 @@ class Category(
     @CreationTimestamp
     @Column(name = "created_date")
     @JsonIgnore
-    val createdDate: LocalDateTime = LocalDateTime.now()
+    val createdDate: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    val category: Category
 )
---
+++ api/financeapi/src/main/resources/db/migration/V2__rename_table.sql
@@ -0,0 +1,1 @@
+alter table tb_expense rename to tb_bank_statement;
